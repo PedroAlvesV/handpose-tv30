ee886092d41c1421c6d0e767eaea8c213a66f286
Pedro Alves Valentim <pedroalvesvalentim@gmail.com>
2021-06-16 04:23:51 -0300

    Added basic implementation of pose recognition

--- finger-counter.py
+++ finger-counter.py
@@ -1,6 +1,5 @@
 import cv2
 import time
-import os
 import HandTrackingModule as htm
 
 def indexOf(finger, distance=0):
@@ -29,77 +28,75 @@ cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
 
-folderPath = "FingerImages"
-myList = os.listdir(folderPath)
-print(myList)
-overlayList = []
-for imPath in myList:
-    image = cv2.imread(f'{folderPath}/{imPath}')
-    # print(f'{folderPath}/{imPath}')
-    overlayList.append(image)
-
-print(len(overlayList))
 pTime = 0
 
 detector = htm.handDetector(maxHands=1, detectionCon=0.75)
 
-tipIds = [4, 8, 12, 16, 20]
+poses = []
+
+# 0 fingers pose
+p = {"FIST": [False]*5}
+poses.append(p)
+
+# 1 finger poses
+p = {
+    "POINT": [False, True, False, False, False],
+    "THUMBS": [True, False, False, False, False]
+}
+poses.append(p)
+
+# 2 fingers poses
+p = {
+    "PEACE": [False, True, True, False, False],
+    "ROCK": [False, True, False, False, True],
+    "SHAKA": [True, False, False, False, True]
+}
+poses.append(p)
+
+# 3 fingers poses
+p = {
+    "THREE": [False, True, True, True, False],
+    "LOVE": [True, True, False, False, True]
+}
+poses.append(p)
+
+# 4 fingers poses
+p = {"FOUR": [False, True, True, True, True]}
+poses.append(p)
+
+# 5 fingers poses
+p = {"OPEN": [True]*5}
+poses.append(p)
+
+def getPose(fingers):
+    totalFingers = fingers.count(True)
+    for pose in poses[totalFingers]:
+        if poses[totalFingers][pose] == fingers:
+            return pose
+    return "UNDEF"
+
+# poderia codificar em binário, também, e usar a própria sequência como chave de dicionário. ex.: "01000" como "POINT"
 
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
     lmList = detector.findPosition(img, draw=False)
-    # print(lmList)
 
     if len(lmList) != 0:
+        
         fingers = []
-
-        # Thumb
-        is_right_hand = lmList[1][1] > lmList[0][1]
-    
-        if is_right_hand:
-            if lmList[tipIds[0]][1] > lmList[tipIds[0] - 2][1]:
-                fingers.append(True)
-            else:
-                fingers.append(False)
-        else: 
-            if lmList[tipIds[0]][1] < lmList[tipIds[0] - 2][1]:
-                fingers.append(True)
-            else:
-                fingers.append(False)
-
-
-        # 4 Fingers
-        for id in range(1, 5):
-            if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
-                fingers.append(True)
-            else:
-                fingers.append(False)
-
-        #print(fingers)
-        totalFingers = fingers.count(True)
-        #print(totalFingers)
-
-        fingers2 = [is_closed(1), is_closed(2), is_closed(3), is_closed(4), is_closed(5)]
-        totalFingers = fingers2.count(False)
-        print(fingers2)
-
-        #h, w, c = overlayList[totalFingers - 1].shape
-        #img[0:h, 0:w] = overlayList[totalFingers - 1]
-
-        cv2.rectangle(img, (20, 225), (170, 425), (0, 255, 0), cv2.FILLED)
-        cv2.putText(img, str(totalFingers), (45, 375), cv2.FONT_HERSHEY_PLAIN,
-                    10, (255, 0, 0), 25)
+        for i in range(1,6):
+            fingers.append(not is_closed(i))
+        print(fingers, getPose(fingers))
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
 
-    cv2.putText(img, f'FPS: {int(fps)}', (400, 70), cv2.FONT_HERSHEY_PLAIN,
-                3, (255, 0, 0), 3)
+    cv2.putText(img, f'FPS: {int(fps)}', (20, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
 
     cv2.imshow("Image", img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q') or cv2.waitKey(1) & 0xFF == ord('Q'):
         break
 
 cap.release()
