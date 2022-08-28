25f5797245b7b1684c91d8121c424673f337ba6f
Pedro Alves Valentim <pedroalvesvalentim@gmail.com>
2022-08-28 13:47:58 -0300

    Moved 'poses' specification to poses.py

--- handpose.py
+++ handpose.py
@@ -4,6 +4,7 @@ import HandTrackingModule as htm
 from sys import argv
 import paho.mqtt.client as mqtt
 import argparse
+from poses import poses
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--debug', action='store_true')
@@ -68,44 +69,7 @@ pTime = 0
 
 detector = htm.handDetector(maxHands=MAX_HANDS, detectionCon=0.75)
 
-poses = []
-
 __UNDEF = "undefined"
-
-# 0 fingers pose
-p = {"FIST": [False]*5}
-poses.append(p)
-
-# 1 finger poses
-p = {
-    "POINT": [False, True, False, False, False],
-    "THUMBS": [True, False, False, False, False]
-}
-poses.append(p)
-
-# 2 fingers poses
-p = {
-    "PEACE": [False, True, True, False, False],
-    "ROCK": [False, True, False, False, True],
-    "SHAKA": [True, False, False, False, True],
-    "LOSER": [True, True, False, False, False]
-}
-poses.append(p)
-
-# 3 fingers poses
-p = {
-    "THREE": [False, True, True, True, False],
-    "LOVE": [True, True, False, False, True]
-}
-poses.append(p)
-
-# 4 fingers poses
-p = {"FOUR": [False, True, True, True, True]}
-poses.append(p)
-
-# 5 fingers poses
-p = {"OPEN": [True]*5}
-poses.append(p)
 
 def getPose(fingers):
     totalFingers = fingers.count(True)
---
+++ poses.py
@@ -0,0 +1,39 @@
+poses = []
+
+# 0 fingers pose
+p = {"FIST": [False]*5}
+poses.append(p)
+
+# 1 finger poses
+p = {
+    "POINT": [False, True, False, False, False],
+    "THUMBS": [True, False, False, False, False],
+    "PINKY": [False, False, False, False, True]
+}
+poses.append(p)
+
+# 2 fingers poses
+p = {
+    "PEACE": [False, True, True, False, False],
+    "ROCK": [False, True, False, False, True],
+    "SHAKA": [True, False, False, False, True],
+    "LOSER": [True, True, False, False, False]
+}
+poses.append(p)
+
+# 3 fingers poses
+p = {
+    "THREE": [False, True, True, True, False],
+    "LOVE": [True, True, False, False, True]
+}
+poses.append(p)
+
+# 4 fingers poses
+p = {
+    "FOUR": [False, True, True, True, True]
+}
+poses.append(p)
+
+# 5 fingers poses
+p = {"OPEN": [True]*5}
+poses.append(p)
